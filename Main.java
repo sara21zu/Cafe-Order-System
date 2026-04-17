4b5510f7031c361d804b4efc93c1adc5c240fc57
sara21zu <sarazu21s@gmail.com>
2026-04-17 13:19:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,28 @@
+// Product
 class Drink {
     String size;
     boolean milk;
     int sugar;
+    boolean chocolate;
 
     public void showDrink() {
         System.out.println("Size: " + size);
         System.out.println("Milk: " + milk);
         System.out.println("Sugar: " + sugar);
+        System.out.println("Chocolate: " + chocolate);
     }
 }
 
+// Builder Interface
 interface DrinkBuilder {
     void setSize(String size);
     void addMilk();
     void setSugar(int sugar);
+    void addChocolate();
     Drink build();
 }
 
+// Coffee Builder
 class CoffeeBuilder implements DrinkBuilder {
     private Drink drink = new Drink();
 
@@ -32,20 +38,78 @@ class CoffeeBuilder implements DrinkBuilder {
         drink.sugar = sugar;
     }
 
+    public void addChocolate() {
+        drink.chocolate = true;
+    }
+
     public Drink build() {
         return drink;
     }
 }
 
+// Tea Builder
+class TeaBuilder implements DrinkBuilder {
+    private Drink drink = new Drink();
+
+    public void setSize(String size) {
+        drink.size = size;
+    }
+
+    public void addMilk() {
+        drink.milk = false; // شاي بدون حليب
+    }
+
+    public void setSugar(int sugar) {
+        drink.sugar = sugar;
+    }
+
+    public void addChocolate() {
+        drink.chocolate = true;
+    }
+
+    public Drink build() {
+        return drink;
+    }
+}
+
+// Abstract Factory
+interface DrinkFactory {
+    DrinkBuilder createDrinkBuilder();
+}
+
+// Coffee Factory
+class CoffeeFactory implements DrinkFactory {
+    public DrinkBuilder createDrinkBuilder() {
+        return new CoffeeBuilder();
+    }
+}
+
+// Tea Factory
+class TeaFactory implements DrinkFactory {
+    public DrinkBuilder createDrinkBuilder() {
+        return new TeaBuilder();
+    }
+}
+
+// Main
 public class Main {
     public static void main(String[] args) {
-        CoffeeBuilder builder = new CoffeeBuilder();
 
+        // اختاري نوع المشروب
+        DrinkFactory factory = new CoffeeFactory(); // أو TeaFactory
+
+        // خذي الـ builder
+        DrinkBuilder builder = factory.createDrinkBuilder();
+
+        // بناء المشروب
         builder.setSize("Large");
         builder.addMilk();
         builder.setSugar(2);
+        builder.addChocolate();
 
-        Drink coffee = builder.build();
-        coffee.showDrink();
+        // النتيجة
+        Drink drink = builder.build();
+        drink.showDrink();
     }
 }
+
